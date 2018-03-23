86f98a993c22a51fc25636894baf9c8194b8c4e7
Long Nguyen <hi@longnv.info>
2018-03-23 16:03:59 +0700

    Update test and readme

---
+++ tests/queue.spec.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var chai_1 = require("chai");
+require("mocha");
+var queue_1 = require("../src/components/queue");
+queue_1.queue.setHander({
+    execute: function (item) {
+        return new Promise((function (resolve) {
+            console.log('Test handler. Executing item:', item);
+            resolve(true);
+        }));
+    }
+});
+queue_1.queue.start();
+describe('Test internal queue', function () {
+    it('Test enqueue', function (done) {
+        queue_1.queue.push([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+        queue_1.queue.push([11, 22, 33, 44]);
+        for (var i = 0; i < 10; i++) {
+            queue_1.queue.push(i);
+        }
+        chai_1.expect(queue_1.queue.getItems().length).to.be.equal(0);
+        done();
+    });
+});
