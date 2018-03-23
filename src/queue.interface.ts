c6dc415fefa5b9a50d9800387fab4522afeba05f
Long Nguyen <hi@longnv.info>
2018-03-23 15:55:21 +0700

    Add queue interface

---
+++ src/queue.interface.ts
@@ -0,0 +1,36 @@
+export interface QueueItemInterface{
+
+}
+export interface QueueHandlerInterface{
+    execute(item: QueueItemInterface): Promise<boolean>;
+}
+export interface QueueInterface{
+    /**
+     *
+     * @param {QueueHandlerInterface} handler
+     * @returns {QueueInterface}
+     */
+    setHander(handler: QueueHandlerInterface): QueueInterface;
+
+    /**
+     *
+     * @param {number} max
+     * @returns {QueueInterface}
+     */
+    setNumberOfExecution(max: number): QueueInterface;
+
+    /**
+     *
+     * @returns {QueueItemInterface[]}
+     */
+    getItems() : QueueItemInterface[];
+
+    /**
+     *
+     * @param {QueueItemInterface} item
+     * @returns {QueueInterface}
+     */
+    push(item: QueueItemInterface): QueueInterface;
+
+    start(): any
+}
